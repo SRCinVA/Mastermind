c08ed1b5ab83e1b8853dfbda44f4571ff0217c7a
SRCinVA <scitarella@hotmail.com>
2023-02-13 20:24:54 -0500

    Add files via upload

--- game.py
+++ game.py
@@ -5,6 +5,12 @@ TRIES = 10
 CODE_LENGTH = 4
 
 def generate_code():
-    pass
+    code = []
+    for _ in range(CODE_LENGTH):  # the underscore is an anonymous variable, in whcih we don't really care about what place we're on. (Seems odd...) 
+        color = random.choice(COLORS) # we randomly select a color ...
+        code.append(color)  # ... which gets appended into [code]
+    return code
 
+def guess_code():
+    guess = input("Guess: ").upper().split(" ")
 
