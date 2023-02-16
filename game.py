85230587d1239243283259b934b0f83bc0f73f2a
SRCinVA <scitarella@hotmail.com>
2023-02-15 20:27:48 -0500

    Completed first method

--- game.py
+++ game.py
@@ -18,9 +18,22 @@ def guess_code():
 
         if len(guess) != CODE_LENGTH:
             print(f"You must guess {CODE_LENGTH} colors") # f strings are available in Python 3.7 and above
-            continue # brings us back to the top of the while loop (NOT the if statement)
+            continue # we break out of the for loop and it brings us back to the top of the while loop
 
         for color in guess:
             if color not in COLORS:
                 print(f"Invalid color: {color}. Try again.")
                 break # where would this put us after breaking out?
+    
+        else: # if you get to this point, it means that all of your colors were correct and you can break out of the while loop.
+            break
+
+    return guess  # when you've broken out of the while loop, now you can return guess
+
+def check_code(guess, real_code):
+    color_counts = {}  # how many of each color you have
+    correct_pos = 0 
+    incorrect_pos = 0
+
+    for color in real_code:
+        pass
